fcc972c9034d8f33e387747c29b28216b8bbedcd
Sam Montoya <samuel.montoya@080n-pdc-09.local>
2016-07-28 14:36:35 -0600

    shitty

--- src/switchsetup/view/MainMenu.java
+++ src/switchsetup/view/MainMenu.java
@@ -18,11 +18,16 @@ public class MainMenu extends javax.swing.JPanel {
      */
     public MainMenu() {
         initComponents();
+        
+        setupLogo(logoLabel);
         setupMenuButton(irfButton, "/resources/netButtonMenu.png", this.getClass());
         setupMenuButton(linkButton, "/resources/netButtonMenu.png", this.getClass());
         setupMenuButton(snmpButton, "/resources/netButtonMenu.png", this.getClass());
         
         setupSmallButton(exitButton, "/resources/netButtonSmall.png", this.getClass());
+        setupSmallButton(changelogButton, "/resources/netButtonSmall.png", this.getClass());
+        setupSmallButton(aboutButton, "/resources/netButtonSmall.png", this.getClass());
+        setupSmallButton(helpButton, "/resources/netButtonSmall.png", this.getClass());
     }
 
     /**
@@ -39,63 +44,91 @@ public class MainMenu extends javax.swing.JPanel {
         linkButton = new javax.swing.JButton();
         snmpButton = new javax.swing.JButton();
         exitButton = new javax.swing.JButton();
+        versionLabel = new javax.swing.JLabel();
+        changelogButton = new javax.swing.JButton();
+        aboutButton = new javax.swing.JButton();
+        helpButton = new javax.swing.JButton();
+
+        setPreferredSize(new java.awt.Dimension(800, 600));
 
         logoLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/netLogo.png"))); // NOI18N
-        logoLabel.setToolTipText("");
 
         irfButton.setText("IRF SETUP");
-        irfButton.setBounds(new java.awt.Rectangle(300, 200, 97, 29));
-        irfButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        irfButton.setPreferredSize(new java.awt.Dimension(400, 180));
 
-        linkButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        linkButton.setLabel("LINK AGGREGATION");
+        linkButton.setText("LINK AGGREGATION");
 
         snmpButton.setText("SNMP SETUP");
-        snmpButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
+        exitButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/netButtonSmall.png"))); // NOI18N
         exitButton.setText("EXIT");
+
+        versionLabel.setText("V: 2.1 [beta 4ever]");
+
+        changelogButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/netButtonSmall.png"))); // NOI18N
+        changelogButton.setText("CHANGELOG");
+
+        aboutButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/netButtonSmall.png"))); // NOI18N
+        aboutButton.setText("ABOUT");
+
+        helpButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/netButtonSmall.png"))); // NOI18N
+        helpButton.setText("ASSISTANCE");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(6, 6, 6)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(irfButton)
+                    .addComponent(linkButton)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(155, 155, 155)
-                        .addComponent(logoLabel))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(89, 89, 89)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(irfButton, javax.swing.GroupLayout.PREFERRED_SIZE, 620, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(linkButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(snmpButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                    .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(91, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE)
+                            .addComponent(snmpButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(changelogButton, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(29, 29, 29)
+                        .addComponent(aboutButton, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(helpButton, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(72, 72, 72)
+                .addComponent(logoLabel)
+                .addGap(140, 140, 140)
+                .addComponent(versionLabel))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(logoLabel)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(logoLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(versionLabel))
+                .addGap(24, 24, 24)
+                .addComponent(irfButton)
                 .addGap(18, 18, 18)
-                .addComponent(irfButton, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(linkButton)
                 .addGap(18, 18, 18)
-                .addComponent(linkButton, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(snmpButton)
                 .addGap(18, 18, 18)
-                .addComponent(snmpButton, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(31, 31, 31)
-                .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(exitButton)
+                    .addComponent(changelogButton)
+                    .addComponent(aboutButton)
+                    .addComponent(helpButton)))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton aboutButton;
+    private javax.swing.JButton changelogButton;
     private javax.swing.JButton exitButton;
+    private javax.swing.JButton helpButton;
     private javax.swing.JButton irfButton;
     private javax.swing.JButton linkButton;
     private javax.swing.JLabel logoLabel;
     private javax.swing.JButton snmpButton;
+    private javax.swing.JLabel versionLabel;
     // End of variables declaration//GEN-END:variables
 }
--- src/switchsetup/view/Setups.java
+++ src/switchsetup/view/Setups.java
@@ -20,7 +20,7 @@ public interface Setups {
     
     static void setupLogo(JLabel pic)
 	{
-			ImageIcon backgroundImage = new ImageIcon(MainMenu.class.getResource("/resources/netLogoLong.png"));
+			ImageIcon backgroundImage = new ImageIcon(MainMenu.class.getResource("/resources/netLogo.png"));
 			Image image = backgroundImage.getImage();
 			image = image.getScaledInstance(450, 175, java.awt.Image.SCALE_SMOOTH);
 			backgroundImage = new ImageIcon(image);
@@ -43,23 +43,22 @@ public interface Setups {
 		button.setVerticalTextPosition(JButton.CENTER);
 		button.setFont(new Font("Neue", Font.BOLD, 17));
                 button.setForeground(Color.DARK_GRAY);
-                button.setBorderPainted(false);
-                button.setFocusPainted(false);
-                button.setBorder(null);
 	}
     
     static void setupSmallButton(JButton button, String pictureLoc, Class componentClass)
 	{
+            button.setHorizontalTextPosition(JButton.CENTER);
+		button.setVerticalTextPosition(JButton.CENTER);
+		button.setFont(new Font("Neue", Font.BOLD, 14));
+		button.setForeground(Color.DARK_GRAY);
+                button.setBorderPainted(false);
+                button.setFocusPainted(false);
+                button.setFocusable(true);
+                button.setContentAreaFilled(false);
 		ImageIcon backgroundImage = new ImageIcon(componentClass.getResource(pictureLoc));
 		Image image = backgroundImage.getImage();
-		image = image.getScaledInstance(150, 50, java.awt.Image.SCALE_FAST);
+		image = image.getScaledInstance(130, 60, java.awt.Image.SCALE_FAST);
 		backgroundImage = new ImageIcon(image);
 		button.setIcon(backgroundImage);
-		
-		button.setBorderPainted(false);
-		button.setHorizontalTextPosition(JButton.CENTER);
-		button.setVerticalTextPosition(JButton.CENTER);
-		button.setFont(new Font("Neue", Font.BOLD, 17));
-		button.setForeground(Color.DARK_GRAY);
 	}
 }
